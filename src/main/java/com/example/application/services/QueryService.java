c1fa92853d450a466ee2f122f2a27dc3918aadb0
TG <tiangooden@gmail.com>
2025-09-18 21:14:45 -0500

    update query to use jdbc template

--- src/main/java/com/example/application/services/QueryService.java
+++ src/main/java/com/example/application/services/QueryService.java
@@ -1,18 +1,14 @@
 package com.example.application.services;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.sql.DataSource;
 
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import com.example.application.models.Document;
@@ -23,35 +19,31 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class QueryService {
-    private final DataSource dataSource;
     private final EmbeddingModel embeddingModel;
     private final OllamaChatModel chatModel;
+    private final JdbcTemplate jdbcTemplate;
 
     public String query(String prompt) throws IOException, InterruptedException {
         EmbeddingResponse embeddingResponse = this.embeddingModel.embedForResponse(List.of(prompt));
         float[] output = embeddingResponse.getResult().getOutput();
-        List<Document> relevantDocs = new ArrayList<>();
-        try (Connection conn = dataSource.getConnection()) {
-            PGvector.addVectorType(conn);
-            String sql = "SELECT * FROM document ORDER BY embedding <-> CAST(? AS vector) LIMIT 5";
-            PreparedStatement ps = conn.prepareStatement(sql);
+        String sql = "SELECT * FROM document ORDER BY embedding <-> CAST(? AS vector) LIMIT 5";
+        RowMapper<Document> rowMapper = (rs, rowNum) -> {
+            PGvector embeddings = (PGvector) rs.getObject("embedding");
+            return Document.builder()
+                    .id(rs.getLong("id"))
+                    .content(rs.getString("content"))
+                    .embedding(embeddings.toArray())
+                    .build();
+        };
+        List<Document> relevantDocs = jdbcTemplate.query(connection -> {
+            PGvector.addVectorType(connection);
+            var ps = connection.prepareStatement(sql);
             ps.setObject(1, new PGvector(output));
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                var embeddings = (PGvector) rs.getObject("embedding");
-                relevantDocs.add(Document.builder()
-                        .id(rs.getLong("id"))
-                        .content(rs.getString("content"))
-                        .embedding(embeddings.toArray())
-                        .build());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            return ps;
+        }, rowMapper);
         String context = relevantDocs.stream()
                 .map(Document::getContent)
                 .collect(Collectors.joining("\n---\n"));
-        String answer = chatModel.call(context + "\n\n" + prompt);
-        return answer;
+        return chatModel.call(context + "\n\n" + prompt);
     }
 }
